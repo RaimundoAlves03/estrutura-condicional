c6d96cb468fca5c5c4c15d2310cd565d87ba476f
Raimundo Alves de Oliveira <rayalves9322@gmail.com>
2025-11-28 18:17:59 -0300

    Add files via upload

---
+++ estrutura condicional.py
@@ -0,0 +1,16 @@
+lista = ["Ana", "Bruno", "Carlos", "Diana", "Eva"]
+
+print(lista)
+nome = input("Digite um nome ds lista: ")
+if nome == "Ana":
+    print("Ola Ana!")
+elif nome == "Bruno":
+    print("Oi Bruno!")
+elif nome == "Carlos":
+    print("E ai Carlos!")
+elif nome == "Diana":
+    print("Bom dia Diana!")
+elif nome == "Eva":
+    print("Boa tarde Eva")
+else:
+    print("O nome informado nao esta na lista")
